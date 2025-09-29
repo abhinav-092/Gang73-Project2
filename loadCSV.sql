8d8f69bc6f77c0021093984d34f5d154897e19bb
Ethan Ma <ethanma53@tamu.edu>
2025-09-29 14:20:19 -0500

    changed the test table

--- loadCSV.sql
+++ loadCSV.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS testTable;
 
-CREATE TABLE testTable (
-    col1 VARCHAR(255),
-    col2 VARCHAR(255)
+CREATE TABLE IF NOT EXISTS testing(
+    test1 VARCHAR(255),
+    test2 VARCHAR(255),
+    test3 VARCHAR(255)
 );
 
-COPY testTable (col1, col2)
+COPY testing (test1, test2, test3)
 FROM 'C:\\Users\\55zay\\Desktop\\CLASSES\\csce331\\test.csv' DELIMITER ',' CSV HEADER;
