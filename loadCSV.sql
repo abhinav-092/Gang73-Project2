bc2ff37ec74d2c1c349c27e67c81ccfe4727af36
zzayyna <sayyed.zayna@tamu.edu>
2025-09-29 14:08:01 -0500

    hi

---
+++ loadCSV.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS testTable;
+
+CREATE TABLE testTable (
+    col1 VARCHAR(255),
+    col2 VARCHAR(255)
+);
+
+COPY testTable (col1, col2)
+FROM 'C:\\Users\\55zay\\Desktop\\CLASSES\\csce331\\test.csv' DELIMITER ',' CSV HEADER;
