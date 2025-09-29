aff87b9e39601d761b0beb9da2c265639580085e
abhinav akavaram <abhinav.akavaram@tamu.edu>
2025-09-29 14:46:23 -0500

    try3?

--- loadCSV.sql
+++ loadCSV.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS testing(
     test3 VARCHAR(255)
 );
 
+<<<<<<< HEAD
 CREATE TABLE IF NOT EXISTS testing2(
     test4 VARCHAR(255),
     test5 VARCHAR(255)
@@ -16,3 +17,9 @@ FROM 'C:\\Users\\55zay\\Desktop\\CLASSES\\csce331\\test.csv' DELIMITER ',' CSV H
 
 COPY testing2 (test4, test5)
 FROM 'C:\\Users\\55zay\\Desktop\\CLASSES\\csce331\\test2.csv' DELIMITER ',' CSV HEADER;
+=======
+COPY testing (test1, test2, test3)
+FROM 'C:\\Users\\55zay\\Desktop\\CLASSES\\csce331\\test.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS pokedex;
+>>>>>>> abhinav_test
