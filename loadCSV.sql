e0c56227f8c4b3915f04f92d6a7f45be26bbadb6
abhinav akavaram <abhinav.akavaram@tamu.edu>
2025-09-29 14:32:15 -0500

    replacing

---
+++ loadCSV.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS testTable;
+
+CREATE TABLE IF NOT EXISTS testing(
+    test1 VARCHAR(255),
+    test2 VARCHAR(255),
+    test3 VARCHAR(255)
+);
+
+COPY testing (test1, test2, test3)
+FROM 'C:\\Users\\55zay\\Desktop\\CLASSES\\csce331\\test.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS pokedex;
