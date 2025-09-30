75d459c9be161e58fb93e1b441020603e8501ab0
abhinav akavaram <abhinav.akavaram@tamu.edu>
2025-09-30 16:40:11 -0500

    got queries working and loading csv files

--- CreateCSVs.sql
+++ CreateCSVs.sql
@@ -3,10 +3,4 @@ DROP TABLE IF EXISTS testtable;
 CREATE TABLE testtable(
     col1 VARCHAR,
     col2 VARCHAR
-<<<<<<< HEAD
 );
-
-\copy testtable(col1, col2) FROM 'testSheet1.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
-=======
-);
->>>>>>> abhinav_test
