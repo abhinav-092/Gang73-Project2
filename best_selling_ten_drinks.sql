ce079b97b3f96789ea82b303e8026d906ee9cd29
Ethan Ma <ethanma53@tamu.edu>
2025-10-01 02:04:44 -0500

    change item_name to drink in output

--- best_selling_ten_drinks.sql
+++ best_selling_ten_drinks.sql
@@ -1,10 +1,10 @@
 SELECT
-  mi.item_name,
+  mi.item_name AS drink,
   COUNT(*) AS sold
 FROM order_summary AS os
 JOIN menu_items AS mi
   ON mi.item_id = os.item_id
 WHERE os.item_id BETWEEN 1 AND 19
 GROUP BY mi.item_name
-ORDER BY sold DESC, mi.item_name
+ORDER BY sold DESC, drink
 LIMIT 10;
